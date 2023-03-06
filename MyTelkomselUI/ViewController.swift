04d2040be545218eb0bf1a2169e9433d39e76152
Carpathianoo <adlanaufar@gmail.com>
2023-03-06 10:51:46 +0700

    Initial Commit

---
+++ MyTelkomselUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTelkomselUI
+//
+//  Created by Adlan Nourindiaz on 06/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
